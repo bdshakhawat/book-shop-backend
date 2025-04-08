8299f80b409e9892ba32794eafdc83012af6a4a6
saifulislam106 <saifulislam106915@gmail.com>
2025-04-08 19:22:07 +0600

    sendResponse reusable fuc creates and apply in book controller

--- src/app/Modules/Book/Book.controller.ts
+++ src/app/Modules/Book/Book.controller.ts
@@ -1,49 +1,59 @@
 import { Request, Response } from "express";
 import { BookServices } from "./Book.services";
+import sendResponse from "../../Utils/sendResponse";
+import httpStatus from "http-status";
 
-const createBook = async (req:Request, res:Response) => {
+const createBook =  async (req:Request, res:Response) => {
     const payload = req.body;
     const result = await BookServices.createBook(payload);
-    res.json({
+    sendResponse(res ,{
+        statusCode: httpStatus.OK,
         message: 'Book created successfully',
-        success: true,
+        suscess: true,
         data: result,
     })
 }
 
 const getAllBooks= async (req:Request, res:Response) => {
     const result = await BookServices.getAllBooks();
-    res.json({
+    sendResponse(res ,{
+        statusCode: httpStatus.OK,
         message: "Books retrieved successfully",
-        success: true,
+        suscess: true,
         data: result,
-    })
+    })  
 }
+
 const getSingleBook= async (req:Request, res:Response) => {
     const {productId} = req.params;
     const result = await BookServices.getSingleBook(productId);
-    res.json({
+    sendResponse(res ,{
+        statusCode: httpStatus.OK,
         message: "Book retrieved successfully",
-        success: true,
+        suscess: true,
         data: result,
     })
 }
+
 const updateBook= async (req:Request, res:Response) => {
     const {productId} = req.params;
     const payload = req.body;
     const result = await BookServices.updateBook(payload, productId);
-    res.json({
-        message: "Book updated successfully",
-        success: true,
+    sendResponse(res ,{
+        statusCode: httpStatus.OK,
+        message: "Books updated successfully",
+        suscess: true,
         data: result,
     })
 }
+
 const deleteBook= async (req:Request, res:Response) => {
     const {productId} = req.params;
     const result = await BookServices.deleteBook(productId);
-    res.json({
-        message: "Book deleted successfully",
-        success: true,
+    sendResponse(res ,{
+        statusCode: httpStatus.OK,
+        message: "Books deleted successfully",
+        suscess: true,
         data: result,
     })
 }

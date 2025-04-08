d00bd7882006a962728f100aec60af71300d6bfe
bdshakhawat <mdshakwat.hossain@gmail.com>
2025-04-08 10:20:44 +0600

    Order controller  created

---
+++ src/app/Modules/Orders/Order.controller.ts
@@ -0,0 +1,75 @@
+// import catchAsync from '../../Utils/catchAsync';
+// import sendResponse from '../../Utils/sendResponse';
+import httpStatus from 'http-status';
+import { orderService } from './Order.services';
+// import { IUser } from '../User/User.interface';
+
+const createOrder = catchAsync(async (req, res) => {
+  const user = req.user;
+
+  const order = await orderService.createOrder(
+    user as IUser,
+    req.body,
+    req.ip!,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'Order placed successfully',
+    data: order,
+  });
+});
+
+const getOrders = catchAsync(async (req, res) => {
+  const order = await orderService.getOrders();
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: 'Order retrieved successfully',
+    data: order,
+  });
+});
+
+const verifyPayment = catchAsync(async (req, res) => {
+  const order = await orderService.verifyPayment(req.query.order_id as string);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: 'Order verified successfully',
+    data: order,
+  });
+});
+
+const changeOrderStatus = catchAsync(async (req, res) => {
+  const order = await orderService.changeOrderStatus(
+    req.params.id,
+    req.query.status as string,
+  );
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: 'Order status changed successfully',
+    data: order,
+  });
+});
+
+const getCustomerOrder = catchAsync(async (req, res) => {
+  const { email } = req.user;
+  const order = await orderService.getCustomerOrdersFromDb(email);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: 'Order retrieved successfully',
+    data: order,
+  });
+});
+export const orderController = {
+  createOrder,
+  verifyPayment,
+  getOrders,
+  changeOrderStatus,
+  getCustomerOrder,
+};

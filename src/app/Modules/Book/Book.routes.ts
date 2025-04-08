494bedcd0355ba69bfed40562d1217a6f28ecc48
saifulislam106 <saifulislam106915@gmail.com>
2025-04-08 13:05:38 +0600

    book.routes file correction

--- src/app/Modules/Book/Book.routes.ts
+++ src/app/Modules/Book/Book.routes.ts
@@ -3,10 +3,12 @@ import { BookControllers } from "./Book.controller";
 
 const route = Router();
 route.get("/:productId", BookControllers.getSingleBook);
-route.get("/:productId", BookControllers.updateBook);
-route.get("/:productId", BookControllers.deleteBook);
+route.patch("/:productId", BookControllers.updateBook);
+route.delete("/:productId", BookControllers.deleteBook);
 route.post("/", BookControllers.createBook);
 route.get("/", BookControllers.getAllBooks);
+
+export const BookRoutes = route;
 
 
 

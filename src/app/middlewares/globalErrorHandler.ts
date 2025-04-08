deab17aa009780497bf4c5c874c4924d315e785c
Badhon40 <cse12005018brur@gmail.com>
2025-04-08 12:01:35 +0600

    errorHandler

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,35 @@
+import { NextFunction } from "express"
+import mongoose from "mongoose"
+import { handleCastError } from "../Errors/CustomError"
+import { handleDuplicateError } from "../Errors/DuplicateError"
+import { handleValidationError } from "../Errors/ValidationError"
+import { handleGenericError } from "../Errors/GenericError"
+
+interface IErrorResponse {
+    success: boolean,
+    name : string,
+    message : string,
+    error : any
+}
+
+export const globalErrorHandler = (err: IErrorResponse , req: Request, res: Response , next: NextFunction)=>{
+    console.log(err)
+
+    if(err instanceof mongoose.Error.CastError){
+        handleCastError(err, res)
+    }
+
+    else if(err.code && err.code===11000){
+       handleDuplicateError(err,res)
+    }
+
+    else if( err instanceof mongoose.Error.ValidationError){
+        handleValidationError(err,res)
+    }
+
+   else if(err instanceof Error){
+     handleGenericError(err,res)
+   }
+     
+}  
+ 

97911b84670ae20fc74263d5467834ae50b7014f
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-04-13 15:47:48 +0600

    update authGurd code

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application } from 'express';
 import router from './app/Routes';
-import {globalErrorHandler} from './app/middlewares/globalErrorHandler';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
 app.use('/api/v1', router);
 
--- src/app/middlewares/authGurd.ts
+++ src/app/middlewares/authGurd.ts
@@ -2,16 +2,16 @@ import { NextFunction, Request, Response } from "express"
 import catchAsync from "../Utils/catchAsync"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import{User} from "../Modules/User/User.model"
+import Config from "../Config"
 
 const authGurd = (...requiredRoles: string[]) => {
     return catchAsync(async (req:Request, res:Response, next:NextFunction)=>{
         const token = req.headers.authorization 
-       console.log(token)
         if(!token){
             throw new Error('you are not authorized')
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload
+        const decoded = jwt.verify(token, Config.jwt_access_secret as string) as JwtPayload
 
         const {email,role} = decoded
 

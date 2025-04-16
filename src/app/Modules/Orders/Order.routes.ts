41b4e009eba446e299522933fd5731a56fe5253c
Md. Shakhawat Hossain <65524069+bdshakhawat@users.noreply.github.com>
2025-04-16 15:49:48 +0600

    Update Order.routes.ts

--- src/app/Modules/Orders/Order.routes.ts
+++ src/app/Modules/Orders/Order.routes.ts
@@ -2,11 +2,9 @@ import { Router } from 'express';
 import authGurd from '../../middlewares/authGurd';
 import { orderController } from './Order.controller';
 const route = Router();
-<<<<<<< HEAD
-// route.patch('/verify-order', authGurd('admin'), orderController.verifyPayment);
-=======
-route.patch('/verify-order', authGurd('admin'));
->>>>>>> e5b96d816497d14b4bc907cb4efdbdd3912ad23c
+
+ route.patch('/verify-order', authGurd('admin'), orderController.verifyPayment);
+
 route.post(
   '/create-order',
   authGurd('user', 'admin'),
